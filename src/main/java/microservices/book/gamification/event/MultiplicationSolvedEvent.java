32d0d51ea19765ed4522aa1602a8bbe51be45a6d
Eman <eman.cse2008@gmail.com>
2020-08-17 08:17:57 +0200

    initial commit

---
+++ src/main/java/microservices/book/gamification/event/MultiplicationSolvedEvent.java
@@ -0,0 +1,28 @@
+package microservices.book.gamification.event;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+/**
+ * Event received when a multiplication has been solved in the system.
+ * Provides some context information about the multiplication.
+ */
+@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+class MultiplicationSolvedEvent implements Serializable {
+    private final Long multiplicationResultAttemptId;
+    private final Long userId;
+    private final boolean correct;
+
+    public MultiplicationSolvedEvent(){
+        // Empty constructor for JSON/JPA
+            this(0L, 0L, false);
+    }
+}
